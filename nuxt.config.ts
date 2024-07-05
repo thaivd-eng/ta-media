6ef46892f243b5ae88299622abf1fa9e2b85ce25
tr1nh <trandiepkhanhtrinh@gmail.com>
2024-07-05 09:47:01 +0000

    Added DaisyUI and Pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: false },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  }
 })
