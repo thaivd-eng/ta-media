f4ca8f41adbf6cb91207667fdf15317203bc72af
Khánh Trình <trandiepkhanhtrinh@gmail.com>
2025-09-12 15:05:23 +0700

    fix: updated BASE_URL in gemini.js
    - User reported that AI responsed can't view video file
    - Updated library GeminiWithFiles in Apps Script to v22 and deployed new API endpoint

--- utils/gemini.js
+++ utils/gemini.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://script.google.com/macros/s/AKfycbxM4LsX0MfXbvGvjGIvzAoyGTwrtq0qJZhObBe_MPxoLV_7p9quGvnqsy23LG2VkTyapQ/exec";
+const BASE_URL = "https://script.google.com/macros/s/AKfycbxfE3czBF6xSMDrKqdEs-rWnB2HY7uv4W2ZB8o10NBG2RT8PNej3n2a0pwciGw0pJ69OQ/exec";
 
 export async function ask(data) {
   console.log('DEBUG API');
